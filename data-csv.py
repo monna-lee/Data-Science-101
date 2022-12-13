2107a14fb9e398fb856f58cef0bc4ebd4eee6d86
monna-lee <monnalee02@gmail.com>
2022-12-13 13:22:23 +0800

    o

---
+++ data-csv.py
@@ -0,0 +1,10 @@
+# import pandas informs the library to import the panadas data analysis library
+# as pd tells python allows a short form version to be easily read for the users
+
+import pandas as pd
+
+# read csv is used to load a CSV file as a pandas dataframe
+# df stands for dataframe and it is a 2 dimensional data structure
+df = dataframe = pd.read_csv('aapl-yahoo.csv')
+
+print(df)
